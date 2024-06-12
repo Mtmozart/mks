925158a58141e36b7c1e356fb44c6d6b3c57ff95
matheus mozart <mmsnborges@gmail.com>
2024-06-11 23:56:04 -0400

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+
+export const EnConfig = {
+  database: {
+    HOST_DB: process.env.HOST_DB,
+    PORT_DB: +process.env.PORT_DB,
+    USER_DB: process.env.USER_DB,
+    PASSWORD_DB: process.env.PASSWORD_DB,
+    NAME_DB: process.env.NAME_DB,
+    URL: process.env.URL_DATABASE,
+  },
+};
